54e1f7f0e08467169a75596ba19012c3b6ef2da1
rabbort <kbutler@knights.ucf.edu>
2015-04-10 08:06:30 -0400

    Player

---
+++ Group1_Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public int hp;
+	public bool alive;
+	
+	private AnimationSelector animSelector;
+
+	// Use this for initialization
+	void Start () {
+		animSelector = GetComponent<AnimationSelector>();
+		hp = 50;
+		alive = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(alive){
+			if(hp <= 0){
+				animSelector.die ();
+				alive = false;
+				print(alive);
+			}
+			else
+				animSelector.animate ();
+		}
+	}
+
+
+	void OnCollisionEnter(Collision collision){
+		//if(collision.collider.tag == "axe"){
+			hp -= 10;
+			print("player hp:"+hp);
+		//}
+	}
+}
