57f956e94b3a78917d0d530d90f60097aa1f3d58
rabbort <kbutler@knights.ucf.edu>
2015-04-10 08:06:57 -0400

    Orc scripts

---
+++ Group1_Final/Assets/Scripts/OrcAttachWeapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrcAttachWeapon : MonoBehaviour {
+	public GameObject hand;
+
+	// Use this for initialization
+	void Start () {
+		hand = GameObject.Find ("MaskedOrcRigWeapon");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.parent = hand.transform;
+	}
+}
---
+++ Group1_Final/Assets/Scripts/OrcController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrcController : MonoBehaviour {
+	public Animation animate;
+	public int hp;
+	public bool alive = true;
+	private bool attack = false;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		animate = GetComponent<Animation>();
+		player = GameObject.Find ("Warrior");
+		hp = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(alive){
+			if(hp <= 0){
+				animate.CrossFade ("die02");
+				alive = false;
+			}
+			else if(attack){
+				animate.CrossFade ("hit");
+			}
+			else if(Vector3.Distance (player.transform.position, transform.position) < 3.0f){
+				animate.CrossFade ("attack01");
+			}
+			else
+				animate.CrossFade ("idle");
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if(player.GetComponent<AnimationSelector>().attacking || collision.collider.tag == "catbullet"){
+			StartCoroutine(underAttack());
+			hp -= 10;
+		}
+	}
+	
+	IEnumerator underAttack(){
+		attack = true;
+		yield return new WaitForSeconds(animate["hit"].length);
+		attack = false;
+		yield break;
+	}
+}
